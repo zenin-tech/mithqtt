324ed10705eda9ed1dbc3009baa4fee8ed203b58
WY <wuyiadmin@163.com>
2017-06-26 16:26:55 +0800

    修改部分验证逻辑和基本的数据处理等

--- mithqtt-authenticator-dummy/src/main/java/io/j1st/mithqtt/authenticator/power/PowerAuthenticator.java
+++ mithqtt-authenticator-dummy/src/main/java/io/j1st/mithqtt/authenticator/power/PowerAuthenticator.java
@@ -3,12 +3,13 @@ package io.j1st.mithqtt.authenticator.power;
 import com.github.longkerdandy.mithqtt.api.auth.Authenticator;
 import com.github.longkerdandy.mithqtt.api.auth.AuthorizeResult;
 import io.j1st.power.storage.mongo.MongoStorage;
+import io.j1st.power.storage.mongo.entity.AgentStatus;
 import io.j1st.power.storage.mongo.entity.ProductStatus;
+import io.j1st.power.storage.mongo.entity.ServiceType;
 import io.netty.handler.codec.mqtt.MqttGrantedQoS;
 import io.netty.handler.codec.mqtt.MqttTopicSubscription;
 import org.apache.commons.configuration.AbstractConfiguration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,18 +45,32 @@ public class PowerAuthenticator implements Authenticator {
     @Override
     public AuthorizeResult authConnect(String clientId, String userName, String password) {
         //验证clentId是否有效
-        if(!mongoStorage.isAgentExists(clientId)) {
+        if (!mongoStorage.isAgentExists(clientId)) {
             return AuthorizeResult.FORBIDDEN;
         }
         //验证用户名密码是否合法
-        if(!mongoStorage.isAgentAuth(userName, password)) {
+        if (!mongoStorage.isAgentAuth(userName, password)) {
             return AuthorizeResult.FORBIDDEN;
         }
         //验证product状态是否正常
         Integer status = this.mongoStorage.getProductStatusByAgentId(clientId);
-        if(status == null || !status.equals(ProductStatus.SERVICE.value())){
+        if (status == null || !status.equals(ProductStatus.SERVICE.value())) {
             return AuthorizeResult.FORBIDDEN;
         }
+       /* //验证所在的service是否链接已满
+        String userId = this.mongoStorage.getOperatorIdByAgent(clientId);
+        if (userId != null) {
+            //link count
+            long count = this.mongoStorage.getServiceCountByOperatorId(ServiceType.HARDWARE_MANAGER.value(), new ObjectId(userId));
+            if (count <= 0) {
+                return AuthorizeResult.FORBIDDEN;
+            }
+        }*/
+        // Validate Agent Connect Privilege
+        if (this.mongoStorage.isDisableAgent(new ObjectId(clientId), AgentStatus.DISABLED.value())) {
+            return AuthorizeResult.FORBIDDEN;
+        }
+
         return AuthorizeResult.OK;
     }
 
@@ -70,9 +85,13 @@ public class PowerAuthenticator implements Authenticator {
 //        if(!topicName.endsWith("upstream")){
 //            return AuthorizeResult.FORBIDDEN;
 //        }
-        //验证product状态是否正常
-        Integer status = this.mongoStorage.getProductStatusByAgentId(clientId);
-        if(status == null || !status.equals(ProductStatus.SERVICE.value())){
+//        //验证product状态是否正常
+//        Integer status = this.mongoStorage.getProductStatusByAgentId(clientId);
+//        if (status == null || !status.equals(ProductStatus.SERVICE.value())) {
+//            return AuthorizeResult.FORBIDDEN;
+//        }
+        // Validate Agent Connect Privilege
+        if (this.mongoStorage.isDisableAgent(new ObjectId(clientId), AgentStatus.DISABLED.value())) {
             return AuthorizeResult.FORBIDDEN;
         }
         return AuthorizeResult.OK;
@@ -85,7 +104,7 @@ public class PowerAuthenticator implements Authenticator {
             if (!this.allowDollar && subscription.topic().startsWith("$")) r.add(MqttGrantedQoS.FAILURE);
             if (subscription.topic().equals(this.deniedTopic)) r.add(MqttGrantedQoS.FAILURE);
             if (!subscription.topic().endsWith("downstream")) r.add(MqttGrantedQoS.FAILURE);
-            if (subscription.topic().indexOf(clientId) == -1) r.add(MqttGrantedQoS.FAILURE);
+            if (!subscription.topic().contains(clientId)) r.add(MqttGrantedQoS.FAILURE);
             r.add(MqttGrantedQoS.valueOf(subscription.requestedQos().value()));
         });
         return r;
